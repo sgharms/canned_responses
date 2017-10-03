e2af74691f44518b862dad523e4534957b44a8de
Steven G. Harms <sgharms+github-notifications@stevengharms.com>
2017-10-03 00:20:31 -0400

    Can pass canned responses on stevengharms.com

--- background.js
+++ background.js
@@ -1,7 +1,46 @@
+const MENU_PREFIX = "CANNED_REPONSE_CONTEXT_MENU-";
+
+var _CANNED_RESPONSE_REGISTRY = {
+}
+
+var _CANNED_RESPONSES = [
+  ["Alpha", "Alpha body"],
+  ["Beta", "Beta body"],
+];
+
 chrome.webNavigation.onCompleted.addListener(function(details) {
   chrome.tabs.executeScript(details.tabId, {
-	file: "canned_response.js"
-  }, () => chrome.tabs.sendMessage(details.tabId, { magic_word: "ROMY ZOMIE" }, txt => alert("called me back: " + txt)) );
+    file: "canned_response.js"
+  }, () => {
+    // Create top-level menu
+    var parentMenuId = chrome.contextMenus.create({
+      id: MENU_PREFIX + details.tabId,
+      title: "Canned Response",
+      contexts: ["editable"],
+      documentUrlPatterns: [ "*://stevengharms.com/*" ]
+    });
+
+    // Create a canned responses menu
+    _CANNED_RESPONSES.forEach((response, i) => {
+      var id = chrome.contextMenus.create({
+        title: response[0],
+        parentId: parentMenuId,
+        id: MENU_PREFIX + details.tabId + i,
+        contexts: ["editable"],
+        documentUrlPatterns: [ "*://stevengharms.com/*" ]
+      })
+      _CANNED_RESPONSE_REGISTRY[id] = response[1];
+    });
+  })
 }, {
   url: [{ "hostContains": "stevengharms.com" }]
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  var boilerplate = _CANNED_RESPONSE_REGISTRY[info.menuItemId];
+  if (boilerplate) {
+    // "Sends a single message to the content script(s) in the specified tab,
+    // with an optional callback to run when a response is sent back."
+    chrome.tabs.sendMessage(tab.id, { canned_response: boilerplate })
+  }
 });
--- canned_response.js
+++ canned_response.js
@@ -1,4 +1,8 @@
-chrome.runtime.onMessage.addListener( (message, sender, cb) => {
-  alert(message.magic_word);
-  cb(document.querySelector("h1.post-title").innerText);
+console.log("running");
+var _CANNED_RESPONSE_ACTIVE_INPUT;
+
+chrome.runtime.onMessage.addListener( (message) => {
+  _CANNED_RESPONSE_ACTIVE_INPUT.value = message.canned_response;
 });
+
+document.querySelector("input[type='text']").addEventListener('focus', e => _CANNED_RESPONSE_ACTIVE_INPUT = e.target);
