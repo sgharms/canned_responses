91de17cb65351e6dcc1085c94eeac914639e7849
Steven G. Harms <sgharms+github-notifications@stevengharms.com>
2017-10-03 18:35:04 -0400

    Prepared for LinkedIn

--- canned_response.js
+++ canned_response.js
@@ -1,8 +1,16 @@
-console.log("running");
 var _CANNED_RESPONSE_ACTIVE_INPUT;
+var _CANNED_RESPONSE_INPUT_ELEMENT = "textarea";
+var _CANNED_RESPONES_RUNTIME_EVENTS_BOUND;
 
-chrome.runtime.onMessage.addListener( (message) => {
-  _CANNED_RESPONSE_ACTIVE_INPUT.value = message.canned_response;
-});
+document.getElementsByTagName(_CANNED_RESPONSE_INPUT_ELEMENT)[0].addEventListener('focus', e => _CANNED_RESPONSE_ACTIVE_INPUT = e.target)
 
-document.querySelector("input[type='text']").addEventListener('focus', e => _CANNED_RESPONSE_ACTIVE_INPUT = e.target);
+if (!_CANNED_RESPONES_RUNTIME_EVENTS_BOUND) {
+  chrome.runtime.onMessage.addListener(message => {
+    if (message.initialize_placeholder) {
+      document.getElementsByTagName(_CANNED_RESPONSE_INPUT_ELEMENT)[0].setAttribute("placeholder", message.initialize_placeholder);
+      return;
+    }
+    _CANNED_RESPONSE_ACTIVE_INPUT.value += message.canned_response;
+  });
+  _CANNED_RESPONES_RUNTIME_EVENTS_BOUND = true;
+}
