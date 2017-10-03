a88239c099b40368718297e9a7b4dff35c19f5f7
Steven G. Harms <sgharms+github-notifications@stevengharms.com>
2017-10-02 23:38:08 -0400

    Basic message passing configured

--- canned_response.js
+++ canned_response.js
@@ -1,1 +1,4 @@
-alert(document.querySelector("h1.post-title").innerText);
+chrome.runtime.onMessage.addListener( (message, sender, cb) => {
+  alert(message.magic_word);
+  cb(document.querySelector("h1.post-title").innerText);
+});
